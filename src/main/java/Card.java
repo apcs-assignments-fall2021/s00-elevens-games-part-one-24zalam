47f4b1ae59458bbe575dd4ab9da8ec413df66bf8
zalam <24zalam@athenian.org>
2022-02-01 09:55:03 -0800

    got my laptop back

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,68 +1,152 @@
-// The Card class represents a single card in our card game
 public class Card {
 
+
 	// String value that holds the rank of the card
+
+
 	private String rank;
 
+
 	// String value that holds the suit of the card
+
+
 	private String suit;
 
+
+
 	// int value that holds the point value
+
+
 	private int pointValue;
 
+
+
+
 	// Constructor: Creates a new Card instance.
+
+
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		// YOUR CODE HERE
+
+
+		this.rank=cardRank;
+
+
+		this.suit=cardSuit;
+
+
+		this.pointValue=cardPointValue;
+
+
 	}
+
+
+
 
 	// Accesses this Card's rank.
+
+
 	public String getRank() {
-		// YOUR CODE HERE
-		return "";
+
+
+		return this.rank;
+
+
 	}
+
+
 
 	// Accesses this Card's suit.
+
+
 	public String getSuit() {
-		// YOUR CODE HERE
-		return "";
+
+
+		return this.suit;
+
+
 	}
+
+
 
 	// Accesses this Card's point value.
+
+
 	public int getPointValue() {
-		// YOUR CODE HERE
-		return -1;
+
+
+		return this.pointValue;
+
+
 	}
 
+
 	// Compare this card with another
+
+
 	public boolean equals(Card other) {
-		// YOUR CODE HERE
+
+
+		if(this.rank.equals(other.rank) && this.suit.equals(other.suit) && this.pointValue == other.getPointValue()){
+
+			return true;
+
+		}
+
 		return false;
+
+
 	}
 
 	// Converts the rank, suit, and point value into a string in the format
+
+
 	//     "[Rank] of [Suit] (point value = [PointValue])".
+
+
 	//     e.g. "Queen of Spades (point value = 10);
+
+
 	public String toString() {
-		// YOUR CODE HERE
-		return "";
+		return (rank + "of" + suit + "(point value =" + pointValue +")");
+
 	}
-
-
-
 
 
 
 	// CODE BELOW HERE IS ALREADY WRITTEN:
+
+
 	// This version of equals is used in the testing code
+
+
 	public boolean equals(Object o) {
+
 		if (this == o) {
+
 			return true;
+
+
 		}
+
+
 		else if (o == null || getClass() != o.getClass()) {
+
 			return false;
+
 		}
+
 		else {
+
 			return this.equals((Card) o);
+
+
 		}
+
+
 	}
+
+
 }
+
+
+
